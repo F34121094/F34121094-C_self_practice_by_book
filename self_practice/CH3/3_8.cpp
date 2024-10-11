967a93111a829e33abd5dc004119cd45700f7239
F34121094 <162594929+F34121094@users.noreply.github.com>
2024-10-11 19:40:54 +0800

    Add files via upload

---
+++ self_practice/CH3/3_8.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	char ch = 'A';
+	
+	printf("ch = %c\n",ch);
+	printf("ASCII of ch = %d\n\n",ch);
+	
+	char a = 70;
+	printf("ch = %c\n",a);
+	printf("ASCII of ch = %d",a);
+	
+	return 0;
+}
