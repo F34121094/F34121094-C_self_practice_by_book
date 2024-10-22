6feb38322694f178ea71130ff3df5eefe2bb81a4
F34121094 <162594929+F34121094@users.noreply.github.com>
2024-10-23 00:01:28 +0800

    Add files via upload

---
+++ self_practice/CH4/4_4.cpp
@@ -0,0 +1,12 @@
+// floor(x) 傳回不大於x的最大整數
+#include<stdio.h>
+#include<math.h>
+int main(){
+	double x1 = 3.5;
+	double x2 = -3.5;
+	
+	printf("floor(x1) = %.2f\n", floor(x1));
+	printf("floor(x2) = %.2f", floor(x2));
+	
+	return 0;
+} 
---
+++ self_practice/CH4/4_5.cpp
@@ -0,0 +1,13 @@
+// ceil(x)傳回不小於x的最小整數
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	double x1 = 3.5;
+	double x2 = -3.5;
+	
+	printf("ceil(x1) = %.2f\n", ceil(x1));
+	printf("ceil(x2) = %.2f", ceil(x2));
+	
+	return 0;
+} 
