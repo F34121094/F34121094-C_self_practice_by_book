3208f3e72b828cf9cdbf920fdadbac2a5e057bf4
F34121094 <162594929+F34121094@users.noreply.github.com>
2024-10-30 16:33:38 +0800

    Add files via upload

---
+++ self_practice/CH11/11_1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	float b;
+	char c;
+	printf("a ªº address = %p\n",&a);
+	printf("sizeof(a) = %d\n",sizeof(a));
+	printf("b ªº address = %p\n",&b);
+	printf("sizeof(b) = %d\n",sizeof(b));
+	printf("c ªº address = %p\n",&c);
+	printf("sizeof(c) = %d\n",sizeof(c));
+	return 0;
+}
